d4279d7d70197fe392f351bf317ab3c54d60ad6d
merulox <merulox@merulox.com>
2023-09-30 20:37:45 -0400

    things

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -1,6 +1,6 @@
 # Edit this configuration file to define what should be installed on
 # your system.  Help is available in the configuration.nix(5) man page
-# and in the NixOS manual (accessible by running ‘nixos-help’).
+# and in the NixOS manual (accessible by running 'nixos-help').
 { config, pkgs, lib, ... }:
 let
   aagl-gtk-on-nix = import (builtins.fetchTarball "https://github.com/ezKEa/aagl-gtk-on-nix/archive/main.tar.gz");
